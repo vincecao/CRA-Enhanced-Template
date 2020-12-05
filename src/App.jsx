3983cff507f150f0813ced394b612e26dac65c62
Lineng Cao <linengca@usc.edu>
2020-12-05 04:04:14 -0800

    add tailwindcss sample on App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,24 @@
 import React from 'react';
 import logo from '~/logo.svg';
-import '~/App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
+    <div className="bg-black h-screen w-full flex justify-center items-center">
+      <div className="text-center">
+        <img src={logo} className="animate-spin h-32 m-auto mb-10" alt="logo" />
+        <p className="text-white text-lg my-5">
+          Edit <code className="p-1 font-mono text-sm bg-gray-800 text-yellow-600 rounded-lg">src/App.jsx</code> and
+          save to reload.
         </p>
-        <a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
+        <a
+          className="text-red-400 uppercase p-2 rounded-lg hover:bg-gray-100 transition duration-200"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
           Learn React
         </a>
-      </header>
+      </div>
     </div>
   );
 }
